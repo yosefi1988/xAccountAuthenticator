c29d0de5958afab4d2478c8486383472bcad1bfc
sajjad yosefi <yosefi1988@gmail.com>
2024-04-14 04:28:47 -0700

    04-14-2024

--- accountauthenticator/src/main/java/ir/sajjadyosefi/accountauthenticator/activity/payments/PaymentActivity.java
+++ accountauthenticator/src/main/java/ir/sajjadyosefi/accountauthenticator/activity/payments/PaymentActivity.java
@@ -265,8 +265,8 @@ public class PaymentActivity extends Activity {
                 .asPaymentRequest(
                         "e8a913e8-f089-11e6-8dec-005056a205be",
                         amountZarrin,
-//                        String.format("%s://%s","return5", "zarinpalpayment5" ),
-                        String.format("%s://%s", AccountGeneral.getSchemezarinpalpayment(), AccountGeneral.getZarinpalpayment()),
+                        String.format("%s://%s", "return", "zarinpalpayment"),
+//                        String.format("%s://%s", AccountGeneral.getSchemezarinpalpayment(), AccountGeneral.getZarinpalpayment()),
                         (discription.length() == 0 ? "" : discription),
                         phone,
                         "yosefi1988@gmail.com"
