3b65b92802fda95658d641ada494fab1f1db056f
adminSajjad <yosefi1988@gmail.com>
2025-01-20 11:09:19 +0330

    v2 new msi

--- accountauthenticator/src/main/java/ir/sajjadyosefi/accountauthenticator/classes/Config.java
+++ accountauthenticator/src/main/java/ir/sajjadyosefi/accountauthenticator/classes/Config.java
@@ -2,7 +2,7 @@ package ir.sajjadyosefi.accountauthenticator.classes;
 
 public class Config {
     //        String url = "http://192.168.1.5:80/Api/User/Login";
-    public static String MAINHOST = "http://test.sajjadyosefi.ir/";
+    public static String MAINHOST = "https://test.sajjadyosefi.ir/";
     public static String SMSHOST = "09999816652";
 
 
--- app/src/main/java/ir/sajjadyosefi/android/xTubeless/networkLayout/networkLayout/Url.java
+++ app/src/main/java/ir/sajjadyosefi/android/xTubeless/networkLayout/networkLayout/Url.java
@@ -10,8 +10,8 @@ public class Url {
     public static final String Telegram                 = "https://t.me/SoftwareEngineerX";
 
     public static final String REST_API_IP_ADDRESS                       = "http://sokht.epolice.ir:7001/";   //internet Dns
-    public static final String REST_API_IP_ADDRESS2                      = "http://test.sajjadyosefi.ir";   //internet Dns
-    public static final String REST_API_IP_ADDRESS_MAIN                  = "http://test.sajjadyosefi.ir/";   //internet Dns
+    public static final String REST_API_IP_ADDRESS2                      = "https://test.sajjadyosefi.ir";   //internet Dns
+    public static final String REST_API_IP_ADDRESS_MAIN                  = "https://test.sajjadyosefi.ir/";   //internet Dns
 
 
 
